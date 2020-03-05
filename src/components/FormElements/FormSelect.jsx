7952fc46a2ec976de3ab0b6671bf80cf058599cb
Starstrider <david.liu987@hotmail.com>
2020-03-06 03:29:57 +0800

    Feat: built formselect element

--- src/components/FormElements/FormSelect.jsx
+++ src/components/FormElements/FormSelect.jsx
@@ -1,14 +1,61 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import "./FormSelect.style.scss";
 
-const FormSelect = ({name}) => {
-  return <div class="form-select">
-      <select name={name}>
-          <option>abc</option>
-          <option>abc</option>
-          <option>abc</option>
-      </select>
-  </div>;
+const options = [
+  { value: "chocolate", label: "Chocolate" },
+  { value: "strawberry", label: "Strawberry" },
+  { value: "vanilla", label: "Vanilla" }
+];
+
+const FormSelect = ({ placeholder, name }) => {
+  const [opened, setOpened] = useState("");
+  const [title, setTitle] = useState("");
+
+  useEffect(() => {
+    if (opened === "") {
+      setOpened("down");
+    }
+  }, [opened]);
+
+  useEffect(() => {
+    if (title === "") {
+      setTitle(placeholder);
+    }
+  }, [title, placeholder]);
+
+  const handleClick = () => {
+    if (opened === "down") setOpened("up");
+    else setOpened("down");
+  };
+
+  const changeValue = (event, value) => {
+    setOpened("down");
+    setTitle(value);
+  }
+
+  return (
+    <div className="form-select">
+      <div className="select-title" onClick={handleClick}>
+        {title}
+        <i className={`fas fa-chevron-${opened}`}></i>
+      </div>
+      <div
+        className="select-body"
+        style={opened === "down" ? { display: "none" } : { display: "block" }}
+      >
+        {options.map((option, index) => (
+          <div
+            className="select-option"
+            key={`${name}option-${index}`}
+            value={option.value}
+            onClick={event => changeValue(event, option.label)}
+          >
+            {option.label}
+          </div>
+        ))}
+      </div>
+    </div>
+  );
 };
 
 export default FormSelect;
