c585f994709f08e20969f56a39793d92fc963948
Starstrider <david.liu987@hotmail.com>
2020-03-07 03:53:05 +0800

    Feat: add breadcrumb on setupprofile page

--- src/pages/SetupProfilePage/SetupProfilePage.jsx
+++ src/pages/SetupProfilePage/SetupProfilePage.jsx
@@ -1,10 +1,12 @@
 import React from "react";
 import "./SetupProfilePage.style.scss";
 import ProfileForm from "../../components/Forms/ProfileForm";
+import Breadcrumbs from "../../components/Breadcrumbs";
 
 const SetupProfilePage = () => {
   return (
     <div className="setup-profile-page">
+      <Breadcrumbs parent={"Home"} child={"Set-up your profile"} />
       <ProfileForm />
     </div>
   );
