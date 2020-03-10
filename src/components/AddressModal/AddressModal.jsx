ea4b8e95c5043599e053ae8cf6fa2032c2f7b879
hectorstudio <tyler.chong707@hotmail.com>
2020-03-10 03:10:52 -0700

    Fix : Removed Warnning code

--- src/components/AddressModal/AddressModal.jsx
+++ src/components/AddressModal/AddressModal.jsx
@@ -1,84 +1,106 @@
-import React, {useState} from 'react'
-import './AddressModal.scss'
-import FormSelect from '../FormElements/FormSelect';
+import React, { useState } from "react";
+import "./AddressModal.scss";
+import FormSelect from "../FormElements/FormSelect";
 
-const AddressModal = (props) => {
-  var subtitle;
-  const [isOpen,setIsOpen] = useState(false);
+const AddressModal = props => {
+  const [isOpen, setIsOpen] = useState(false);
   function openModal() {
     setIsOpen(true);
   }
-  function closeModal(){
+  function closeModal() {
     setIsOpen(false);
   }
-  function afterOpenModal() {
-    // references are now sync'd and can be accessed.
-  }
-  const [address, setAddress] = useState(
-  [{
-      id: '1',
-      street: '9a Newcastle Street, Rose Bay 2029'
+
+  const address = [
+    {
+      id: "1",
+      street: "9a Newcastle Street, Rose Bay 2029"
     },
     {
-      id: '2',
-      street: '10B Random Street'
-    }]
-  )
-  
-  const handleChange = () => {
+      id: "2",
+      street: "10B Random Street"
+    }
+  ];
 
-  }
+  const handleChange = () => {};
 
-  const handleInputChange = () => {
-    
-  }
+  const handleInputChange = () => {};
   return (
     <div className="my-addresses">
       <p className="my-addresses__title">My addresses</p>
-        <ul className="my-addresses-list">
-          {address.map(addr => (
-            <li className="my-addresses-list__item" key={addr.id}>
-              <span className="my-addresses-list__text-info">{addr.street}</span> 
-              <div>
-                <span className="my-addresses-list__text-functionality edit">Edit</span>
-                <span className="my-addresses-list__text-functionality">Delete</span>
-              </div>
-              
-            </li>
-          ))}
-        </ul>
-      <button className="my-addresses__btn" onClick={openModal}>Add new address</button>
-        {isOpen &&
-          <div className="styles_overlay">
-            <div className="styles_modal modal_Center" role="dialog" aria-modal="true">
-              <h2 className="my-addresses-modal__title custom-title">Add new address</h2>
-              <i className="fas fa-times" onClick={closeModal}></i>
-              <form className="my-addresses-modal-form">
-                <div className="wrapper">
-                  <label className="label-for-input">Street address</label>
-                  <input required="" type="text" className="addresses-input__item custom-input" value="sdfasdf" onChange={handleInputChange} />
-                </div>
-                <div className="addresses-select__item">
-                  <p className="addresses-select__item-title">City</p>
-                  <FormSelect placeholder="Select" name="subcategory" onChange={handleChange} />
-                </div>
-                <div className="addresses-select__item">
-                  <p className="addresses-select__item-title">State/Province</p>
-                  <FormSelect placeholder="Select" name="subcategory" onChange={handleChange} />
-                </div>
-                <div className="addresses-select__item">
-                  <p className="addresses-select__item-title">Country</p>
-                  <FormSelect placeholder="Select" name="subcategory" onChange={handleChange} />
-                </div>
-                <button type="button" className="my-addresses-modal-form__btn">Send address</button>
-              </form>
+      <ul className="my-addresses-list">
+        {address.map(addr => (
+          <li className="my-addresses-list__item" key={addr.id}>
+            <span className="my-addresses-list__text-info">{addr.street}</span>
+            <div>
+              <span className="my-addresses-list__text-functionality edit">
+                Edit
+              </span>
+              <span className="my-addresses-list__text-functionality">
+                Delete
+              </span>
             </div>
-            
+          </li>
+        ))}
+      </ul>
+      <button className="my-addresses__btn" onClick={openModal}>
+        Add new address
+      </button>
+      {isOpen && (
+        <div className="styles_overlay">
+          <div
+            className="styles_modal modal_Center"
+            role="dialog"
+            aria-modal="true"
+          >
+            <h2 className="my-addresses-modal__title custom-title">
+              Add new address
+            </h2>
+            <i className="fas fa-times" onClick={closeModal}></i>
+            <form className="my-addresses-modal-form">
+              <div className="wrapper">
+                <label className="label-for-input">Street address</label>
+                <input
+                  required=""
+                  type="text"
+                  className="addresses-input__item custom-input"
+                  value="sdfasdf"
+                  onChange={handleInputChange}
+                />
+              </div>
+              <div className="addresses-select__item">
+                <p className="addresses-select__item-title">City</p>
+                <FormSelect
+                  placeholder="Select"
+                  name="subcategory"
+                  onChange={handleChange}
+                />
+              </div>
+              <div className="addresses-select__item">
+                <p className="addresses-select__item-title">State/Province</p>
+                <FormSelect
+                  placeholder="Select"
+                  name="subcategory"
+                  onChange={handleChange}
+                />
+              </div>
+              <div className="addresses-select__item">
+                <p className="addresses-select__item-title">Country</p>
+                <FormSelect
+                  placeholder="Select"
+                  name="subcategory"
+                  onChange={handleChange}
+                />
+              </div>
+              <button type="button" className="my-addresses-modal-form__btn">
+                Send address
+              </button>
+            </form>
           </div>
-        }
+        </div>
+      )}
     </div>
-    
-  )
-}
+  );
+};
 
-export default AddressModal
+export default AddressModal;
--- src/components/SelectProduct/SelectProduct.jsx
+++ src/components/SelectProduct/SelectProduct.jsx
@@ -1,23 +1,17 @@
-import React from 'react'
-import './SelectProduct.scss'
-import FormInput from "../../components/FormElements/FormInput"
-import FormSelect from "../../components/FormElements/FormSelect"
+import React from "react";
+import "./SelectProduct.scss";
+import FormSelect from "../../components/FormElements/FormSelect";
 import UserButton from "../../components/Buttons/UserButton";
 
 const SelectProduct = () => {
-  const action = () => {
-    
-  }
-  const handleClick = () => {
-
-  }
-  const handleChange = () => {
-
-  }
+  const handleClick = () => {};
+  const handleChange = () => {};
 
   return (
     <div className="select-products">
-      <p className="select-products__title">Select products you’re interested in</p>
+      <p className="select-products__title">
+        Select products you’re interested in
+      </p>
       <div className="select-products-block">
         <div className="search-container__items select-category-products">
           <FormSelect placeholder="Select a category" name="category" />
@@ -27,14 +21,25 @@ const SelectProduct = () => {
         </div>
         <div className="search-container__items input-products">
           <div className="ui input select-products-block__input">
-            <input placeholder="Or type product name here…" type="text" value="" onChange={handleChange} />
+            <input
+              placeholder="Or type product name here…"
+              type="text"
+              value=""
+              onChange={handleChange}
+            />
           </div>
-          <UserButton varient="add-search-products" text="Add" action={handleClick}  />
+          <UserButton
+            varient="add-search-products"
+            text="Add"
+            action={handleClick}
+          />
         </div>
       </div>
-      <button className="select-products_btn disabled" type="button">Save changes</button>
+      <button className="select-products_btn disabled" type="button">
+        Save changes
+      </button>
     </div>
-  )
-}
+  );
+};
 
 export default SelectProduct;
