2e3c43381a2d1913ce4726423d4e8bb82457bdd8
Starstrider <david.liu987@hotmail.com>
2020-03-12 21:42:05 +0800

    Fix: simple modification on userbutton, formselect component

--- src/components/FormElements/FormSelect.jsx
+++ src/components/FormElements/FormSelect.jsx
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from "react";
+import React, { useState, useEffect, useRef } from "react";
 import "./FormSelect.style.scss";
 
 const options = [
@@ -7,9 +7,27 @@ const options = [
   { value: "vanilla", label: "Vanilla" }
 ];
 
-const FormSelect = ({ placeholder, name }) => {
+const useOutsideDetect = (ref1, handleClick) => {
+  const handleClickOutside = (event) => {
+    if (ref1.current && !ref1.current.contains(event.target)) {
+      handleClick(false);
+    }
+  }
+  useEffect(() => {
+    // Bind the event listener
+    document.addEventListener("mousedown", handleClickOutside);
+    return () => {
+      // Unbind the event listener on clean up
+      document.removeEventListener("mousedown", handleClickOutside);
+    };
+  });
+};
+
+const FormSelect = ({ placeholder, defaultValue, name, setValue }) => {
   const [opened, setOpened] = useState("");
   const [title, setTitle] = useState("");
+
+  const selectDrop = useRef(null);
 
   useEffect(() => {
     if (opened === "") {
@@ -18,23 +36,37 @@ const FormSelect = ({ placeholder, name }) => {
   }, [opened]);
 
   useEffect(() => {
-    if (title === "") {
+    if (title === "" && defaultValue === "") {
       setTitle(placeholder);
     }
-  }, [title, placeholder]);
+    else if(title === "" && defaultValue !== ""){
+      setTitle(defaultValue);
+    }
+    else if(title !== "" && defaultValue === ""){
+      setTitle(placeholder);
+    }
+  }, [title, placeholder, defaultValue]);
 
-  const handleClick = () => {
-    if (opened === "down") setOpened("up");
-    else setOpened("down");
+  const handleClick = event => {
+    if (opened === "down") {
+      //selectRef.current.focus();
+      setOpened("up");
+    } else setOpened("down");
   };
 
   const changeValue = (event, value) => {
     setOpened("down");
     setTitle(value);
+    setValue(value);
   };
 
   return (
-    <div className="form-select">
+    <div
+      className="form-select"
+      onBlur={() => {
+        console.log("blur event");
+      }}
+    >
       <div className="select-title" onClick={handleClick}>
         {title}
         <i className="icon-dropdown"></i>
