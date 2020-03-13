890d167673c723ee11075e4e0496f0c9ad8d5224
Starstrider <david.liu987@hotmail.com>
2020-03-14 00:09:40 +0800

    Feat:built productsfiltercontainer

---
+++ src/sections/ProductsStack/ProductsFilterContainer.jsx
@@ -0,0 +1,16 @@
+import React from "react";
+import FilterSubItems from "../../components/ProductsStack/FilterSubItems";
+import "./ProductsFilterContainer.style.scss";
+
+const items = ["All", "Sunscreen", "Lotions", "Creams", "Sprays", "Gels", "Body Wash"];
+
+const ProductFilterContainer = () => {
+  return (
+    <div className="products-filter">
+      <FilterSubItems title={"Price"} items={items}/>
+      <FilterSubItems title={"Sub-category"} items={items}/>
+      <FilterSubItems title={"Shipped From"} items={items}/>
+    </div>
+  );
+};
+export default ProductFilterContainer;
