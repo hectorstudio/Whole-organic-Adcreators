cc045efa62052b5f829df9b914134e8bb2cfefe0
Starstrider <david.liu987@hotmail.com>
2020-03-16 00:38:55 +0800

    Fix: use component in cart page

--- src/pages/Account/CartPage/CartPage.jsx
+++ src/pages/Account/CartPage/CartPage.jsx
@@ -1,30 +1,92 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import Breadcrumb from "../../../components/Breadcrumbs/Breadcrumbs";
+import CartListContainer from "../../../components/Cart/CartListContainer";
+import OrderSummary from "../../../components/Cart/OrderSummary";
 import "./CartPage.scss";
 
 const breadCrumbs = [
   {
-    id: '1',
-    title: 'Home',
-    path: '/',
+    id: "1",
+    title: "Home",
+    path: "/",
     active: ""
   },
   {
-    id: '2',
-    title: 'My Cart',
-    path: '/cart',
+    id: "2",
+    title: "My Cart",
+    path: "/cart",
     active: "active"
   }
-]
+];
+
+const mockupCartItems = [
+  {
+    id: "1",
+    img: "../images/product-photo-1.jpg",
+    price: 56.99,
+    title: "Yellow plastic chair",
+    subtitle: "Furniture",
+    info: "From Location here",
+    slug: "name of seller",
+    titleLink: "name of seller",
+    iconDefault: "icon-heart",
+    iconFill: "icon-heart-fill"
+  },
+  {
+    id: "2",
+    img: "../images/product-photo-2.jpg",
+    price: 56.99,
+    title: "Yellow plastic chair",
+    subtitle: "Beauty",
+    info: "From Location here",
+    slug: "name of seller",
+    titleLink: "name of seller",
+    iconDefault: "icon-heart",
+    iconFill: "icon-heart-fill"
+  }
+];
+
+const mockupCartInfo = [
+  {
+    id: "1",
+    quantities: 3
+  },
+  {
+    id: "2",
+    quantities: 1
+  }
+];
 
 const CartPage = () => {
+  const [cartItems, setCartItems] = useState([]);
+  const [cartInfo, setCartInfo] = useState([]);
+
+  useEffect(() => {
+    if (cartItems.length < 1) {
+      setCartItems(mockupCartItems);
+      setCartInfo(mockupCartInfo);
+    }
+  }, [cartItems, cartInfo]);
+
+  const removeItem = value => {
+    console.log("Remove Item:", value);
+  };
+
+  const clickCheckout = value => {
+    console.log("Click Checkout");
+  };
+
   return (
-    <div>
+    <div className="cart-list-container">
       <Breadcrumb breadcrumbsItems={breadCrumbs} />
-      <h2>My Cart</h2>
+      <h2 className="cart-title">My Cart{cartItems.length}</h2>
       <div className="row">
-        <div className="cart-list"></div>
-        <div className="order-summmary"></div>
+        <CartListContainer
+          items={cartItems}
+          cartInfo={cartInfo}
+          removeItem={removeItem}
+        />
+        <OrderSummary cartInfo={cartInfo} clickCheckout={clickCheckout} />
       </div>
     </div>
   );
