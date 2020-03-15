d10e475922f3313bbd3dd321e501405561a9e032
Starstrider <david.liu987@hotmail.com>
2020-03-16 00:38:11 +0800

    Feat:built order summary component

---
+++ src/components/Cart/OrderSummary.jsx
@@ -0,0 +1,8 @@
+import React from "react";
+import "./OrderSummary.style.scss";
+
+const OrderSummary = () => {
+  return <div className="order-summary">OrderSummary</div>;
+};
+
+export default OrderSummary;
