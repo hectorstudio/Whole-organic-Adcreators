342e151c5676be8c91d36b4d7e1b047fa200ade6
Starstrider <david.liu987@hotmail.com>
2020-03-16 23:36:41 +0800

    Feat:built order summary container

--- src/components/Cart/OrderSummary.jsx
+++ src/components/Cart/OrderSummary.jsx
@@ -1,8 +1,26 @@
 import React from "react";
 import "./OrderSummary.style.scss";
 
-const OrderSummary = () => {
-  return <div className="order-summary">OrderSummary</div>;
+const OrderSummary = ({ cartInfo, clickCheckout }) => {
+  return (<div className="order-summary">
+    <button className="btn-checkout">Go to Checkout</button>
+    <div className="cart-sub-total">
+      <div className="cart-sub-item">
+        <span>Item(3)</span>
+        <span>$33.14</span>
+      </div>
+      <div className="cart-sub-item">
+        <span>Shipping</span>
+        <span>Free</span>
+      </div>
+    </div>
+    <div className="cart-total">
+      <div className="cart-sub-item">
+        <span>Total</span>
+        <span>$33.14</span>
+      </div>
+    </div>
+  </div>);
 };
 
 export default OrderSummary;
