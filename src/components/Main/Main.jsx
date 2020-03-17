170faa0486b764d84afc07dc8186b958824e36c0
Starstrider <david.liu987@hotmail.com>
2020-03-17 10:16:18 +0800

    Feat: completed checkouut page

--- src/components/Main/Main.jsx
+++ src/components/Main/Main.jsx
@@ -1,76 +1,82 @@
-import React from 'react'
-import { Switch, Route } from 'react-router'
-import HomePage from '../../pages/Home/HomePage/HomePage'
-import CartPage from '../../pages/Account/CartPage/CartPage'
-import AboutPage from '../../pages/Home/AboutPage/AboutPage'
-import LoginPage from '../../pages/Auth/LoginPage/LoginPage'
-import RegisterPage from '../../pages/Auth/RegisterPage/RegisterPage'
-import SetupProfilePage from '../../pages/Account/SetupProfilePage/SetupProfilePage'
-import ForgotPassPage from '../../pages/Auth/ForgotPassPage/ForgotPassPage'
-import CreatePassPage from '../../pages/Auth/CreatePassPage/CreatePassPage'
-import MyAccountPage from '../../pages/Account/MyAccountPage'
-import './Main.scss';
+import React from "react";
+import { Switch, Route } from "react-router";
+import HomePage from "../../pages/Home/HomePage/HomePage";
+import CartPage from "../../pages/Account/CartPage/CartPage";
+import AboutPage from "../../pages/Home/AboutPage/AboutPage";
+import LoginPage from "../../pages/Auth/LoginPage/LoginPage";
+import RegisterPage from "../../pages/Auth/RegisterPage/RegisterPage";
+import SetupProfilePage from "../../pages/Account/SetupProfilePage/SetupProfilePage";
+import ForgotPassPage from "../../pages/Auth/ForgotPassPage/ForgotPassPage";
+import CreatePassPage from "../../pages/Auth/CreatePassPage/CreatePassPage";
+import MyAccountPage from "../../pages/Account/MyAccountPage";
+import CheckoutPage from "../../pages/Account/CartPage/CheckoutPage";
+import "./Main.scss";
 
 const routes = [
   {
-    id: '1',
-    path: '/',
+    id: "1",
+    path: "/",
     exact: true,
-    title: 'Home',
+    title: "Home",
     component: HomePage
   },
   {
-    id: '2',
-    path: '/about',
-    title: 'About',
+    id: "2",
+    path: "/about",
+    title: "About",
     component: AboutPage
   },
   {
-    id: '3',
-    path: '/cart',
-    title: 'My Cart',
+    id: "3",
+    path: "/cart",
+    title: "My Cart",
     component: CartPage
   },
   {
-    id: '4',
-    path: '/login',
-    title: 'Login',
+    id: "4",
+    path: "/login",
+    title: "Login",
     component: LoginPage
   },
   {
-    id: '5',
-    path: '/register',
-    title: 'Register',
+    id: "5",
+    path: "/register",
+    title: "Register",
     component: RegisterPage
   },
   {
-    id: '6',
-    path: '/profilesetup',
-    title: 'ProfileSetup',
+    id: "6",
+    path: "/profilesetup",
+    title: "ProfileSetup",
     component: SetupProfilePage
   },
   {
-    id: '7',
-    path: '/forgot-pass',
-    title: 'Forgot password',
+    id: "7",
+    path: "/forgot-pass",
+    title: "Forgot password",
     component: ForgotPassPage
   },
   {
-    id: '8',
-    path: '/create-pass',
-    title: 'Create a new password',
+    id: "8",
+    path: "/create-pass",
+    title: "Create a new password",
     component: CreatePassPage
   },
   {
-    id: '9',
-    path: '/my-account',
+    id: "9",
+    path: "/my-account",
     component: MyAccountPage
   },
+  {
+    id: "10",
+    path: "/checkout",
+    component: CheckoutPage
+  }
 ];
 
 const Main = () => {
   return (
-    <main className={'main'}>
+    <main className={"main"}>
       <div className="container">
         <Switch>
           {routes.map(item => (
---
+++ src/pages/Account/CartPage/CheckoutPage.jsx
@@ -0,0 +1,155 @@
+import React, { useState, useEffect } from "react";
+import Breadcrumb from "../../../components/Breadcrumbs/Breadcrumbs";
+import FormInput from "../../../components/FormElements/FormInput";
+import UserButton from "../../../components/Buttons/UserButton";
+import OrderSummary from "../../../components/Cart/OrderSummary";
+import Radio from "@material-ui/core/Radio";
+import { withStyles } from "@material-ui/core/styles";
+import { green } from "@material-ui/core/colors";
+
+import "./CartPage.scss";
+
+const GreenRadio = withStyles({
+  root: {
+    color: green[400],
+    "&$checked": {
+      color: green[600]
+    }
+  },
+  checked: {}
+})(props => <Radio color="default" {...props} />);
+
+const breadCrumbs = [
+  {
+    id: "1",
+    title: "Home",
+    path: "/",
+    active: ""
+  },
+  {
+    id: "2",
+    title: "My Cart",
+    path: "/cart",
+    active: "active"
+  }
+];
+
+const mockupCartInfo = [
+  {
+    id: "1",
+    quantities: 3
+  },
+  {
+    id: "2",
+    quantities: 1
+  }
+];
+
+const visaLogo = require('../../../images/visa.png');
+const masterLogo = require('../../../images/master.png');
+const paypalLogo = require('../../../images/paypal.png');
+
+const CheckoutPage = () => {
+  const [cartInfo, setCartInfo] = useState([]);
+  const [payment, setPayment] = useState("");
+
+  useEffect(() => {
+    if (cartInfo.length < 1) {
+      setCartInfo(mockupCartInfo);
+    }
+  }, [cartInfo]);
+
+  const clickCheckout = () => {
+    console.log("click Checkout");
+  };
+  
+  const changeAction = () => {
+    console.log("change action");
+  }
+
+  const clickAction = () => {
+    console.log("click action");
+  }
+
+  const handleChange = (event, value) => {
+    setPayment(value);
+  }
+
+  return (
+    <div className="cart-list-container">
+      <Breadcrumb breadcrumbsItems={breadCrumbs} />
+      <h2 className="cart-title">My Cart({cartInfo.length})</h2>
+      <div className="row cart-body">
+        <div className="checkout-body">
+          <div className="section payment-method">
+            <div className="title">Pay with</div>
+              <div className="payment-item">
+                <GreenRadio
+                  checked={payment === 'visa'}
+                  onChange={event => handleChange(event, "visa")}
+                  value={payment}
+                  name="radio-button-demo"
+                  inputProps={{ 'aria-label': 'visa' }}
+                />
+                <img src={visaLogo} alt="payment visa" />
+              </div>
+              <div className="payment-item">
+                <GreenRadio
+                  checked={payment === 'master'}
+                  onChange={event => handleChange(event, "master")}
+                  value={payment}
+                  name="radio-button-demo"
+                  inputProps={{ 'aria-label': 'master' }}
+                />
+                <img src={masterLogo} alt="payment master" />
+              </div>
+              <div className="payment-item">
+                <GreenRadio
+                  checked={payment === 'paypal'}
+                  onChange={event => handleChange(event, "paypal")}
+                  value={payment}
+                  name="radio-button-demo"
+                  inputProps={{ 'aria-label': 'paypal' }}
+                />
+                <img src={paypalLogo} alt="payment paypal" />
+              </div>
+              
+          </div>
+          <div className="section payment-address">
+            <div className="title">Send to</div>
+            <div className="address">
+              <div className="address-item">
+                <span>
+                  Rony Chiha
+                  <br />
+                  L1, 45 Bowman St
+                  <br />
+                  Pyrmont, New South Wales 2009
+                  <br />
+                  Australia
+                  <br />
+                  47xxxxx44
+                </span>      
+                <i className="icon-dropdown" />
+              </div>
+              <div className="address-action">
+                <span>Edit</span>
+              </div>
+            </div>
+            <UserButton text="Add new address" varient="primary" disabled={false} action={clickAction}/>
+          </div>
+          <div className="section add-coupons">
+            <div className="title">Add coupons</div>
+            <div className="coupon-form">
+              <FormInput placeholder="Enter code" alert={false} changeAction={changeAction} />
+              <UserButton text="Apply" varient="primary" disabled={false} action={clickAction}/>
+            </div>
+          </div>
+        </div>
+        <OrderSummary cartInfo={cartInfo} clickCheckout={clickCheckout} />
+      </div>
+    </div>
+  );
+};
+
+export default CheckoutPage;
